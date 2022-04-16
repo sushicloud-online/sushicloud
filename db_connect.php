f15bf59ada7f08b8831d998881db6a15d84e908f
Mark Mauro <markymauro@gmail.com>
2022-04-16 14:49:56 -0400

    adding homepage

--- db_connect.php
+++ db_connect.php
@@ -6,7 +6,6 @@
 
   	// set the PDO error mode to exception
   	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database Connection Successful";
 	} 	
 
 	//outputs error if db connection fails
