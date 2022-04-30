27575e7ef1f363798f677c5b60f123440ece2973
Mark Mauro <markymauro@gmail.com>
2022-04-30 19:14:38 -0400

    fixing navbars for good...hopefully

--- admin/manage_admin.php
+++ admin/manage_admin.php
@@ -73,7 +73,7 @@ $results = $query->fetchAll();
 
 <body class="bg-light">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="admin_homepage.php">sushicloud</a>
             <div class="collapse navbar-collapse" id="navbarNav">
