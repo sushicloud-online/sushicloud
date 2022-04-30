6a731d786cbd93c27e53d8ba5ac52a8ecb4ab311
Brianna <briesarmiento@gmail.com>
2022-04-30 19:05:17 -0400

    created manage admin pages

--- admin/manage_admin.php
+++ admin/manage_admin.php
@@ -43,6 +43,12 @@ else if (isset($_SESSION['new_log']) && $_SESSION['new_log'] == true) {
     unset($_SESSION['mi_err']);
 }
 
+//prepares and executes search statement
+$query = $db->prepare('select username, email, first_name, last_name from admin');
+$query->execute();
+
+//gets all anime
+$results = $query->fetchAll();
 ?>
 
 <!doctype html>
@@ -57,6 +63,12 @@ else if (isset($_SESSION['new_log']) && $_SESSION['new_log'] == true) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
     <title>sushicloud</title>
+    <style>
+        .table-hover tbody tr:hover td,
+        .table-hover tbody tr:hover th {
+            background-color: lightsalmon;
+        }
+    </style>
 </head>
 
 <body class="bg-light">
@@ -80,18 +92,54 @@ else if (isset($_SESSION['new_log']) && $_SESSION['new_log'] == true) {
     <div class="container">
 
         <div class="text-center mx-auto mt-5">
-            <img src="../assets/sushicloud.png" width="600px" height="200px" alt="sushicloud">
+            <img src="../assets/sushicloud.png" width="300px" height="100px" alt="sushicloud">
+            <h2 class="mt-2">Manage Users</h2>
         </div>
 
-        <div class="row offset mt-5">
+        <div class="row offset mt-3">
             <center>
-                <a href="manage_anime.php" class="btn btn-dark btn-lg active" role="button">Manage Anime</a>
-                <a href="manage_users.php" class="btn btn-dark btn-lg active" role="button">Manage Users</a>
-                <a href="manage_admin.php" class="btn btn-dark btn-lg active" role="button">Manage Admin</a>
-            </center>   
+                <a href="admin_register.php" class="btn btn-dark btn active" role="button">Insert Admin</a>
+                <!-- <a href="#" class="btn btn-dark btn active" role="button">Edit Anime</a> -->
+            </center>
         </div>
-
     </div>
+    <div class="container py-5 h-50">
+        <div class="row justify-content-center align-items-center h-100">
+            <div class="col-12 col-lg-11 col-xl-13">
+                <div class="card shadow-2-strong card-manage" style="border-radius: 15px;">
+                    <div class="card-body p-4 p-md-5">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Email</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <?php
+                            foreach ($results as $row) {
+                                echo "<form action='./background_scripts/delete_admin_validation.php' method='post'>
+                                    <td>" . $row['username'] . "</td>
+                                    <td>" . $row['email'] . "</td>
+                                    <td>" . $row['first_name'] . "</td>
+                                    <td>" . $row['last_name'] . "</td>
+                                    <td>
+                                        <input type='hidden' name='user' value='" . $row['username'] . "'>
+                                        <input type='submit' value='Delete' class='btn btn-danger'>
+                                        </form>
+                                    </td>
+                                </tr>";
+                            }
+                            ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
 
